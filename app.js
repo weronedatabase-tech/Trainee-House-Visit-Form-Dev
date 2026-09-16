ff222209a7e2f047956e57f887c95d361fb2cb20
weronedatabase-tech <werone.database@gmail.com>
2026-09-16 13:29:29 +0800

    Change environment to development mode

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // 1. FRONTEND ENVIRONMENT TOGGLE
 // Change this single value to "Dev" or "Prod"
 // =====================================================================
-const ENVIRONMENT = "Prod"; 
+const ENVIRONMENT = "Dev"; 
 
 const DEV_API_URL = 'https://script.google.com/macros/s/AKfycbzsU73hNlYm9vqAY4Mn80s_6KMP79eLCi11u8d56NkO_1iDp7a0ew09OWOdvfhzK75T/exec';
 const PROD_API_URL = 'https://script.google.com/macros/s/AKfycbw21ZGdd-SfmRJrB-zMcfzVKTzIG-hU-BwKaA33J1bukq-4_ZJGQfH6_KBr4LdgjZvXmw/exec';
